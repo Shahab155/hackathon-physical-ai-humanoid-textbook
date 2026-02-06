0c7ffc6013ca820d89e032a17889c33b273365d1
Shahab155 <bhuttoshahab1544@gmail.com>
2026-02-06 23:59:19 +0500

    update

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -19,8 +19,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="https://hackathon-physical-ai-humanoid-textbook.vercel.app/docs/module-1/intro">
+            Start Reading 
           </Link>
         </div>
       </div>
